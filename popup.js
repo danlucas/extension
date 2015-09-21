e122d4bdb0fc410fcf9537fe117d2356020ee9b2
Melissa <mjblavos@gmail.com>
2015-09-21 12:14:06 -0400

    edited conditional function

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
+var myUrl = urlFinder();
 
 function urlFinder () {
-	if $("#button") = "one" {
-		return "www.bronto.com";
+	if ($("#button") === "one") {
+		return "http://app.bronto.com";
 	}
-	else if $(#button) = "two" {
-		return "www.google.com";
+	else if ($("#button") === "two") {
+		return "http://bronto.com/welcomekit";
 	} else {
-		return "www.facebook.com";
+		return "https://server.iad.liveperson.net/hc/77739214/?cmd=file&file=visitorWantsToChat&site=77739214&byhref=1&SESSIONVAR!skill=Lead%20Qualification&imageUrl=https://server.iad.liveperson.nethttps://hosting-source.bronto.com/7894/public/LivePerson/";
 	}
 }
 
 function awesome() {
-  chrome.tabs.create({url: urlFinder});
+  chrome.tabs.create({url: myUrl});
 }  
 
 function clickHandler(e) {
